5cea362b5ff37187975933b19c65b103e6e98c6f
k6ss21 <61103379+k6ss21@users.noreply.github.com>
2023-05-02 21:49:21 +0530

    Abitlity Enemy Damage Debuff  added

---
+++ Assets/Ab_EnemyDamageDeBuff.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class Ab_EnemyDamageDeBuff : MonoBehaviour
+{
+     public float cooldownTime;
+     public float debuffMulti;
+     public float debuffTime;
+    bool coolDown;
+    Button button;
+    public static event Action<float, float> OnEnemyDamageDeBuffTrigger;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        coolDown = false;
+    }
+    public void EnemyDamageDeBuffCall()
+    {
+        if (!coolDown)
+        {
+            Debug.Log("EnemyDamageDeBuff Trigger Active...");
+            OnEnemyDamageDeBuffTrigger?.Invoke(debuffMulti, debuffTime);
+            coolDown = true;
+            button.enabled = false;
+            StartCoroutine(CoolDownRoutine());
+
+        }
+        else
+        {
+            Debug.Log(" EnemyDamageDeBuff CoolDown");
+        }
+        IEnumerator CoolDownRoutine()
+        {
+
+            yield return new WaitForSeconds(cooldownTime);
+            coolDown = false;
+            button.enabled = true;
+
+        }
+    }
+}
--- Assets/Script/Enemy/EnemyBoss_1_AttackCollider.cs
+++ Assets/Script/Enemy/EnemyBoss_1_AttackCollider.cs
@@ -7,10 +7,31 @@ public class EnemyBoss_1_AttackCollider : MonoBehaviour
     public float damage;
     private bool isDamaged = false;
 
-
-    public void SetDamageValue(float value)
+    void OnEnable()
     {
-        damage = value;
+        Ab_EnemyDamageDeBuff.OnEnemyDamageDeBuffTrigger += Ab_EnemyDamageDeBuffInitiate;
+    }
+
+    void OnDisable()
+    {
+        Ab_EnemyDamageDeBuff.OnEnemyDamageDeBuffTrigger -= Ab_EnemyDamageDeBuffInitiate;
+    }
+
+    void Ab_EnemyDamageDeBuffInitiate(float valueMulti, float time)
+    {
+        SetDamageValue(valueMulti);
+
+    }
+
+    IEnumerator RevertDamageValue(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SetDamageValue(1);
+    }
+
+    public void SetDamageValue(float valueMulti)
+    {
+        damage *= valueMulti;
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
